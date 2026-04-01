4b84b50cfdda8807a65f3f7e13c48e6c34fa6d16
lab151-32 <lab151@gmail.com>
2026-04-01 10:23:17 +0700

    pertemuan 3

---
+++ app/src/main/java/com/example/crybabyapps/pertemuan3/ThirdActivity.kt
@@ -0,0 +1,43 @@
+package com.example.crybabyapps.pertemuan3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.crybabyapps.R
+import com.example.crybabyapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val nomor = binding.inputNoTujuan.text
+            Log.e("Klik btnKirim", "Tombol berhasil di tekan. Isi dari inputNoTujuan = $nomor")
+
+            Toast.makeText(this, "Anda telah melakukan klik pada tombol Submit", Toast.LENGTH_SHORT)
+                .show()
+
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
+
