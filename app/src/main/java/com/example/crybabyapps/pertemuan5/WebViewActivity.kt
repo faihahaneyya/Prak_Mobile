d52e67f3d9a85d488c1aefa75ded2b83fa0c6006
faihahaneyya <faiha24si@mahasiswa.pcr.ac.id>
2026-04-15 22:48:38 +0700

    before p5

---
+++ app/src/main/java/com/example/crybabyapps/pertemuan5/WebViewActivity.kt
@@ -0,0 +1,61 @@
+package com.example.crybabyapps.pertemuan5
+
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.crybabyapps.R
+import com.example.crybabyapps.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            title = "Web Merdeka"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+
+            setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        }
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://merdeka.com")
+
+
+        binding.webView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+            if (scrollY > oldScrollY) {
+                binding.appBar.setExpanded(false, true) // sembunyikan toolbar
+            } else if (scrollY < oldScrollY) {
+                binding.appBar.setExpanded(true, true) // tampilkan toolbar
+            }
+        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            finish()
+        }
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
