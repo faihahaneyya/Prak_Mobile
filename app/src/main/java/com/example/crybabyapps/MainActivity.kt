f7dc50f571b419baa717b2a4a6a90059b1911873
lab151-32 <lab151@gmail.com>
2026-04-29 08:43:58 +0700

    pertemuan 7 Toolbar dan Fragment

--- app/src/main/java/com/example/crybabyapps/MainActivity.kt
+++ app/src/main/java/com/example/crybabyapps/MainActivity.kt
@@ -9,10 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.crybabyapps.databinding.ActivityMainBinding
+import com.example.crybabyapps.pertemuan3.ThirdActivity
 import com.example.crybabyapps.pertemuan4.FourthActivity
+import com.example.crybabyapps.pertemuan5.FifthActivity
+import com.example.crybabyapps.pertemuan_7.SeventhActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,43 +24,54 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
 
+
+        binding.btnToThird.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+        }
         binding.btnToFourth.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
             intent.putExtra("name", "Politeknik Caltex Riau")
             intent.putExtra("from", "Rumbai")
             intent.putExtra("age", 25)
             startActivity(intent)
+            finish()
         }
-
+        binding.btnToFifth.setOnClickListener {
+            val intent = Intent(this, FifthActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnToSeventh.setOnClickListener {
+            val intent = Intent(this, SeventhActivity::class.java)
+            startActivity(intent)
+        }
         binding.btnLogout.setOnClickListener {
             MaterialAlertDialogBuilder(this)
-                .setTitle("Konfirmasi")
-                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setTitle("Logout")
+                .setMessage("Apakah Anda yakin ingin logout?")
                 .setPositiveButton("Ya") { dialog, _ ->
-
                     val editor = sharedPref.edit()
                     editor.clear()
                     editor.apply()
                     dialog.dismiss()
+
                     val intent = Intent(this, AuthActivity::class.java)
                     startActivity(intent)
                     finish()
-
                 }
-                .setNegativeButton("Batal") { dialog, _ ->
+                .setNegativeButton("Tidak") { dialog, _ ->
                     dialog.dismiss()
-                    Log.e("Info Dialog","Anda memilih Tidak!")
+
                 }
                 .show()
-            }
         }
-
     }
+}
---
+++ app/src/main/java/com/example/crybabyapps/pertemuan_7/SeventhActivity.kt
@@ -0,0 +1,76 @@
+package com.example.crybabyapps.pertemuan_7
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.crybabyapps.R
+import com.example.crybabyapps.databinding.ActivityFifthBinding
+import com.example.crybabyapps.databinding.ActivitySeventhBinding
+import kotlin.text.replace
+
+class SeventhActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySeventhBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySeventhBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setSupportActionBar(binding.toolbar)
+
+        // Menghilangkan judul bawaan agar judul kustom di tengah bisa tampil
+        supportActionBar?.apply {
+            title = "activity seven"
+            subtitle = "Pertemuan ke 7"
+            setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
+        // Menampilkan fragment pertama secara default
+        replaceFragment(SatuFragment())
+
+        // Setup event click untuk mengganti fragment
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(SatuFragment())
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(DuaFragment())
+        }
+
+        binding.btnFragment3.setOnClickListener {
+            replaceFragment(TigaFragment())
+        }
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
