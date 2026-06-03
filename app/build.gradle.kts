4f9f95e6c5d7d3adee28470bbd200850c7a0100a
lab151-32 <lab151@gmail.com>
2026-06-03 08:12:02 +0700

    Pertemuan 11 Onboard Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 }
 
 dependencies {
+    implementation("com.tbuonomo:dotsindicator:5.1.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.gridlayout:gridlayout:1.1.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
---
+++ app/src/main/java/com/example/crybabyapps/Message/Tutorial/TutorialMessageActivity.kt
@@ -0,0 +1,41 @@
+package com.example.crybabyapps.Message.Tutorial
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.crybabyapps.R
+import com.example.crybabyapps.databinding.ActivityAuthBinding
+import com.example.crybabyapps.databinding.ActivityTutorialMessageBinding
+
+class TutorialMessageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTutorialMessageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTutorialMessageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        // Setup ViewPager dengan adapter
+        val fragmentsList = listOf(Tutorial1Fragment(), Tutorial2Fragment(), Tutorial3Fragment())
+        val adapter = TutorialFragmentAdapter(this, fragmentsList)
+        binding.tutorialMessageViewPager.adapter = adapter
+
+        binding.dotIndicator.attachTo(binding.tutorialMessageViewPager)
+    }
+    }
+
+
+
+
+
+
